1797436d72a7fabcd388c659650220840a1321bf
Michael Myers <moek@devmoek.com>
2023-11-23 00:12:52 +0400

    add all files

---
+++ src/components/Balance.vue
@@ -0,0 +1,15 @@
+<template>
+    <h4>Your Balance</h4>
+    <strong id="balance">${{ total }}</strong>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  total: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
